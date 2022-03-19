23f07eceaf9eea873da1d3cb7c2e16ddd40e6f1a
joshivinod <joshivinod@github.com>
2022-03-19 20:12:28 +0530

    Initial Commit

---
+++ src/math.py
@@ -0,0 +1,17 @@
+def add(x, y):
+    return x + y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def subtract(x, y):
+    return x - y;
+
+if __name__ == '__main__':
+    print(add(1, 2))
+    print(multiply(1, 2))
+    print(divide(1, 2))
+    print(subtract(1, 2))
